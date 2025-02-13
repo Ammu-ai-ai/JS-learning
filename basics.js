22bf50b4ac9d51c38f365020519815075adf35ca
TRUPTI1203 <truptimayeesuar@gmail.com>
2025-02-13 23:45:28 +0530

    Truptu Mayee created js

---
+++ basics.js
@@ -0,0 +1,15 @@
+let text = `This is
+a multiline
+string`;
+console.log(text);
+//Backticks are used to define multiline strings without using escape characters.
+
+//single quotes ''
+//let msg='javascript's beauty is immaculate';
+//WE cant use quotations inside a string
+//the interpreter considers the second line as end quote which makes the rest of the lines invalid.
+//double quotes
+let msg = "Javascript's beauty is immaculate";
+//you can also use a forward slash to escape the quote
+//the single quotes have more readability than the double quotes
+//there is no use of triple quotes
